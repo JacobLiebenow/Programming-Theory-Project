82b74d9e9b5904261cb13db7e230dcba4952f146
Jacob Liebenow <liebenow.jacob@gmail.com>
2024-06-21 18:00:32 -0400

    Main Menu V0_2
    Added:
    -Settings menu (currently only visual)
    -Exit Game functionality, swapping between menus functionality

--- Assets/Scripts/UI/Title Screen/UIMainMenuManager.cs
+++ Assets/Scripts/UI/Title Screen/UIMainMenuManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class UIMainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenuScreen;
-    [SerializeField] private GameObject settingsScreen;
+    [SerializeField] private GameObject settingsMenuScreen;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +26,42 @@ public class UIMainMenuManager : MonoBehaviour
     private void SetMainMenuActive()
     {
         mainMenuScreen.SetActive(true);
-        settingsScreen.SetActive(false);
+        settingsMenuScreen.SetActive(false);
 
     }
 
     private void SetSettingsMenuActive()
     {
-        settingsScreen.SetActive(true);
+        settingsMenuScreen.SetActive(true);
         mainMenuScreen.SetActive(false);
+    }
+
+    public void OnNewGameClicked()
+    {
+
+    }
+
+    public void OnLoadGameClicked()
+    {
+
+    }
+
+    public void OnSettingsClicked()
+    {
+        SetSettingsMenuActive();
+    }
+
+    public void OnExitGameClicked()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+Application.Quit();
+#endif
+    }
+
+    public void OnReturnToMainMenuClicked()
+    {
+        SetMainMenuActive();
     }
 }
